257c01a91ca435d0abcd23dcb787e585e795270f
Marc <marcllopis89@gmail.com>
2021-03-18 12:32:38 +0100

    Code for livecoding sesion

---
+++ index.js
@@ -0,0 +1,149 @@
+let weirdPeople = [
+  {
+    name: 'JHASBD',
+    living: 'AJHSDHJABSD',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRz7L26mZeMjYhNBvYVnG0R6b7WaaXwWGz2dQ&usqp=CAU',
+    hobbies: ['Running', 'Games', 'Coding']
+  },
+  {
+    name: 'TFXFTXFX',
+    living: 'jcnjdnfdnf',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREyUFUZJS6ffZx-eaeBnkCNDkusil0dLKxSw&usqp=CAU',
+    hobbies: ['Dancing', 'Beer', 'Sports']
+  },
+]
+
+let students = [
+  {
+    name: 'Alba',
+    living: 'Madrid',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRz7L26mZeMjYhNBvYVnG0R6b7WaaXwWGz2dQ&usqp=CAU',
+    hobbies: ['Running', 'Games', 'Coding']
+  },
+  {
+    name: 'Sara',
+    living: 'Barcelona',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREyUFUZJS6ffZx-eaeBnkCNDkusil0dLKxSw&usqp=CAU',
+    hobbies: ['Dancing', 'Beer', 'Sports']
+  },
+  {
+    name: 'Sergio',
+    living: 'Madrid',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFfo5vMUf8N8QfkKjUIaJHjLMvxHajQgiAXA&usqp=CAU',
+    hobbies: ['Coding', 'Dancing', 'Beer']
+  },
+  {
+    name: 'Javier',
+    living: 'Madrid',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFswRIpsAe1k37Mn9Ve8Xe2fLdby2MfXuxVQ&usqp=CAU',
+    hobbies: ['Running', 'Dancing', 'Beer']
+  },
+  {
+    name: 'Azhar',
+    living: 'Madrid',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://cdn.akc.org/content/hero/funny_dog_pictures_header.jpg',
+    hobbies: ['Games', 'Coding', 'Beer']
+  },
+  {
+    name: 'Zobia',
+    living: 'Barcelona',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRz7L26mZeMjYhNBvYVnG0R6b7WaaXwWGz2dQ&usqp=CAU',
+    hobbies: ['Games', 'Dancing', 'Beer']
+  },
+  {
+    name: 'Virginia',
+    living: 'Barcelona',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://cdn.akc.org/content/hero/funny_dog_pictures_header.jpg',
+    hobbies: ['Games', 'Dancing', 'Beer']
+  },
+  {
+    name: 'Romaisa',
+    living: 'Barcelona',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRz7L26mZeMjYhNBvYVnG0R6b7WaaXwWGz2dQ&usqp=CAU',
+    hobbies: ['Games', 'Dancing', 'Beer']
+  },
+  {
+    name: 'Manuel',
+    living: 'Madrid',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREyUFUZJS6ffZx-eaeBnkCNDkusil0dLKxSw&usqp=CAU',
+    hobbies: ['Dancing', 'Beer', 'Sports']
+  },
+  {
+    name: 'Andreas',
+    living: 'Barcelona',
+    shortAbout: 'Short loin sausage rump doner drumstick. Jerky venison strip steak, andouille porchetta cow frankfurter leberkas sirloin pork meatloaf cupim alcatra pig. T-bone leberkas jowl porchetta, filet mignon spare ribs tail kevin.',
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFfo5vMUf8N8QfkKjUIaJHjLMvxHajQgiAXA&usqp=CAU',
+    hobbies: ['Coding', 'Dancing', 'Beer']
+  },
+]
+
+// example of accumulating values with a for loop
+// let number = 0;
+
+// for(let i = 0; i < 10; i++) {
+//   console.log('this is the ' + i + ' iteration')
+//   console.log('the value of number is ' + number)
+//   console.log('this is the math we are doing: ' + i + ' + ' + number)
+//   number = i + number
+//   console.log('this is the value of number after we leave the ' + i + ' iteration: ' + number)
+// }
+
+// console.log('the result of number after for loop is ',number)
+
+
+
+
+// get the element from the DOM
+let classContainer = document.getElementById('classroom')
+let addStudentsButton = document.getElementById('add')
+
+let classroomToHtml = ''
+
+
+const convertTagToHtml = function(array, tag) {
+  let arrayToHtml = ''
+  for (let i = 0; i < array.length; i++) {
+    arrayToHtml += `<${tag}>${array[i]}</${tag}>`
+  }
+  return arrayToHtml;
+}
+
+
+function addStudents(arrayOfStudents) {
+  // generate as many cards as students are in the students variable
+  for (let i = 0; i < arrayOfStudents.length; i++) {
+    classroomToHtml += `
+  <div class='card-student'>
+    <img src='${arrayOfStudents[i].picture}' alt='${arrayOfStudents[i].name}' class='img-student'>
+    <h3>${arrayOfStudents[i].name} <i>living in ${arrayOfStudents[i].living}</i></h3>
+    <p>${arrayOfStudents[i].shortAbout}</p>
+    <ul>
+      ${convertTagToHtml(arrayOfStudents[i].hobbies, 'li')}
+    </ul>
+    <button class='btn-student'>Know more</button>
+  </div>
+  `
+  }
+  classContainer.innerHTML = classroomToHtml
+}
+
+
+
+addStudentsButton.addEventListener('click', function () {
+  // call the fn that adds students to the DOM
+  addStudents(students)
+  addStudentsButton.disabled = 'disabled'
+})
+
+
